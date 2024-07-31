d77d119f9df914e3d75c9e6c23774d05ed261c7d
Documenter.jl <documenter@juliadocs.github.io>
2024-07-31 16:26:33 +0000

    build based on 8619bef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleContinuation","category":"page"},{"location":"#SimpleContinuation","page":"Home","title":"SimpleContinuation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimpleContinuation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleContinuation]","category":"page"}]
+}
